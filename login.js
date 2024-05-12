9b1a8e331b0003ac391b58bf3e6e0d7996ebea72
jerrychen08 <113333064+jerrychen08@users.noreply.github.com>
2024-05-13 01:51:41 +0800

    登入的前後端程式碼 KJ 會再改良
    登入的前後端程式碼 KJ 會再改良

---
+++ login.js
@@ -0,0 +1,55 @@
+const express = require('express');//使用express
+const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise'); 
+
+const app = express();
+const port = 3000; //設定後端伺服port
+let con ;
+// 連接到MySQL資料庫
+async function connectToMySQL(){
+ con = await mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'secondhandshop',
+  database: 'mydb'
+});
+console.log("連接成功"); 
+}
+
+//解析请求体
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//這段程式碼用於設置跨來源資源共享 (CORS) 的相關標頭，以允許瀏覽器從不同的來源訪問伺服器端的資源，同時保持安全性
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header('Access-Control-Allow-Methods','PUT,GET,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers","X-Requested-With");
+    res.header('Access-Control-Allow-Headers','Content-Type');
+    next();
+})
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const query = 'SELECT * FROM user WHERE email = ? AND password = ?';
+    try {
+      // 使用 con.execute() 代替 db.query()
+      const [results, fields] = await con.execute(query, [email, password]);
+      if (results.length === 1) {
+        //res.send('Login successful');
+        res.json({ message: 'Login successful' });
+      } else {
+        res.status(401).json({ message: 'Invalid email or password' });
+        //res.status(401).send('Invalid email or password' );
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+      //res.status(500).send('Internal server error' );
+    }
+  });
+// 啟動伺服器  指令node 檔名  ex. node app.js  關掉 ctrl + c
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
+
+connectToMySQL(); //連接MySQL
---
+++ login.vue
@@ -0,0 +1,85 @@
+<script>
+    import axios from 'axios';
+    export default{
+        name:'login',
+        data(){
+            return{
+                email:'',
+                password:'',
+                error:''
+            };
+        },
+        methods:{
+            async login(){
+                try{
+                    await axios.post('http://localhost:3000/login/',{
+                        email:this.email,
+                        password:this.password
+                    });
+                alert('User login successfully');
+                }catch(error){
+                    //console.error('Login Error:', error);;
+                    //alert('Login Error: ' + error.message);
+                    this.error = error.response.data.message;
+                    alert('Login Error: '+ this.error);
+                }
+            }
+        }
+    }
+    
+</script>
+
+<template>
+    <body class="text-center">
+    
+    <main class="form-signin">
+      <form class="login col-6 mx-auto mt-5" > 
+        
+        <h1 class="h3 mb-3 fw-normal">登入</h1>
+        <div class="d-flex justify-content-center">
+            <div class="form-floating mb-3 col-6">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" v-model="email">
+            <label for="floatingInput">Email address</label>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            <div class="form-floating mb-3 col-6">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" v-model="password">
+            <label for="floatingPassword">Password</label>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center mb-3">
+            <button class="btn btn-sm btn-outline-success col-3 circle" type="submit" @click="login">登入</button> <!--新增@click="login"-->
+            <div class="mx-1"></div>
+            <RouterLink to="/Register" class="btn btn-sm btn-outline-warning col-3 circle">註冊</RouterLink>
+        </div>
+        <div>
+            <button class="btn btn-sm btn btn-outline-secondary col-3 circle">
+            <RouterLink to="/" class="nav-link px-3">首頁</RouterLink>
+            </button>
+        </div>
+
+        
+      </form>
+    </main>
+    
+    </body>
+</template>
+
+<style >
+
+.login{
+    background-color:#FAEBD7;
+    padding: 40px;
+    box-shadow: 5px 5px 10px 	#D8D8EB;
+}
+.form-control{
+    outline: none !important;
+    box-shadow: none !important;
+    border-color: #D8D8EB;
+}
+.circle{
+    border-radius: 25px;
+}
+
+</style>
