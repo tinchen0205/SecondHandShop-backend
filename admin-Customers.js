4a1e620aba37bf77e5b138bf4403dc3966b63203
tin.chen <s1101726@gm.pu.edu.tw>
2024-05-15 17:50:35 +0800

    新增customers .js

---
+++ admin-Customers.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
+const cors = require('cors'); // 使用 cors 中間件
+
+const app = express();
+const port = 3000; // 設定後端伺服 port
+let con;
+
+// 連接到 MySQL 資料庫
+async function connectToMySQL() {
+  con = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345678', // 修改為你的密碼
+    database: 'mydb' // 修改為你的資料庫名稱
+  });
+  console.log("連接成功admin");
+}
+
+connectToMySQL(); // 連接 MySQL
+
+// 設置 CORS
+app.use(cors());
+
+// 解析請求體
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// 獲取所有使用者數據
+app.get('/Customers', async (req, res) => {
+  try {                                                 //改成你自己的
+    const [rows] = await con.execute('SELECT email, name FROM accounts');
+    res.json(rows);
+  } catch (error) {
+    console.error('Error fetching users: ' + error.stack);
+    res.status(500).send('Error fetching users');
+  }
+});
+
+// 啟動伺服器
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
